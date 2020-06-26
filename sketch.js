54d735203d414900c24803ec21b8f447bacb39d2
Angélica Albuquerque <angelica.o.albuquerque@gmail.com>
2020-06-25 23:09:29 -0300

    Trilha sonora do jogo

--- sketch.js
+++ sketch.js
@@ -5,11 +5,13 @@ let cenario;
 function preload() {
   imagemCenario = loadImage("imagens/cenario/floresta.png");
   imagemPersonagem = loadImage("imagens/personagem/correndo.png");
+  somDoJogo = loadSound("sons/trilha_jogo.mp3");
 }
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   cenario = new Cenario(imagemCenario, 3);
+  somDoJogo.loop();
 }
 
 function draw() {
