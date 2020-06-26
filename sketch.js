b86f200c9dbb1e99c7124bfe872fe5e42f042a70
Angélica Albuquerque <angelica.o.albuquerque@gmail.com>
2020-06-25 22:55:49 -0300

    Background animado / gameloop

--- sketch.js
+++ sketch.js
@@ -8,12 +8,13 @@ function preload() {
 }
 
 function setup() {
-  cenario = new Cenario(imagemCenario, 50);
   createCanvas(windowWidth, windowHeight);
+  cenario = new Cenario(imagemCenario, 3);
 }
 
 function draw() {
   cenario.exibe();
+  cenario.move();
   image(imagemPersonagem, 0, height - 135, 110, 135, 0, 0, 220, 270);
 }
 
@@ -21,10 +22,25 @@ class Cenario {
   constructor(imagem, velocidade) {
     this.imagem = imagem;
     this.velocidade = velocidade;
+    this.x1 = 0;
+    this.x2 = width;
   }
 
   exibe() {
-    background(this.imagem, -this.velocidade, 0, width, height);
-    background(this.imagem, width - this.velocidade + 1, 0, width, height);
+    image(this.imagem, this.x1, 0, width, height);
+    image(this.imagem, this.x2, 0, width, height);
+  }
+
+  move() {
+    this.x1 = this.x1 - this.velocidade;
+    this.x2 = this.x2 - this.velocidade;
+
+    if (this.x1 < -width) {
+      this.x1 = width;
+    }
+
+    if (this.x2 < -width) {
+      this.x2 = width;
+    }
   }
 }
