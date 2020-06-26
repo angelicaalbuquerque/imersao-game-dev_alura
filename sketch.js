b69486419be845bd5775fbd47b3871582b3dd610
Angélica Albuquerque <angelica.o.albuquerque@gmail.com>
2020-06-25 22:00:01 -0300

    background imagemCenario

--- sketch.js
+++ sketch.js
@@ -1,8 +1,14 @@
+let imagemCenario;
+
+function preload() {
+  imagemCenario = loadImage("imagens/cenario/floresta.png");
+}
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
 }
 
 function draw() {
-  background(220);
+  background(imagemCenario);
   circle(mouseX, mouseY, 200);
 }
