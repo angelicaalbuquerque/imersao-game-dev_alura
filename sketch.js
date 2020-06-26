31ed839f4406368245c5a89972c38c5bb708379e
Angélica Albuquerque <angelica.o.albuquerque@gmail.com>
2020-06-25 23:17:45 -0300

    Organização de código

--- sketch.js
+++ sketch.js
@@ -1,6 +1,7 @@
 let imagemCenario;
 let imagemPersonagem;
 let cenario;
+let personagem;
 
 function preload() {
   imagemCenario = loadImage("imagens/cenario/floresta.png");
@@ -11,38 +12,12 @@ function preload() {
 function setup() {
   createCanvas(windowWidth, windowHeight);
   cenario = new Cenario(imagemCenario, 3);
+  personagem = new Personagem(imagemPersonagem);
   somDoJogo.loop();
 }
 
 function draw() {
   cenario.exibe();
   cenario.move();
-  image(imagemPersonagem, 0, height - 135, 110, 135, 0, 0, 220, 270);
-}
-
-class Cenario {
-  constructor(imagem, velocidade) {
-    this.imagem = imagem;
-    this.velocidade = velocidade;
-    this.x1 = 0;
-    this.x2 = width;
-  }
-
-  exibe() {
-    image(this.imagem, this.x1, 0, width, height);
-    image(this.imagem, this.x2, 0, width, height);
-  }
-
-  move() {
-    this.x1 = this.x1 - this.velocidade;
-    this.x2 = this.x2 - this.velocidade;
-
-    if (this.x1 < -width) {
-      this.x1 = width;
-    }
-
-    if (this.x2 < -width) {
-      this.x2 = width;
-    }
-  }
+  personagem.exibe();
 }
