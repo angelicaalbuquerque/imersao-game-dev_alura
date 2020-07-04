aca171dfaa1b7359797340262e2af71349d3d76d
Angélica Albuquerque <angelica.o.albuquerque@gmail.com>
2020-07-03 22:17:57 -0300

    Organização de draw

--- scripts/cenas/jogo.js
+++ scripts/cenas/jogo.js
@@ -64,4 +64,35 @@ class Jogo {
       somDoPulo.play();
     }
   }
+
+  draw() {
+    cenario.exibe();
+    cenario.move();
+
+    pontuacao.exibe();
+    pontuacao.adicionarPonto();
+
+    personagem.exibe();
+    personagem.aplicaGravidade();
+
+    const inimigo = inimigos[inimigoAtual];
+    const inimigoVisivel = inimigo.x < -inimigo.largura;
+
+    inimigo.exibe();
+    inimigo.move();
+
+    if (inimigoVisivel) {
+      inimigoAtual++;
+
+      if (inimigoAtual > 2) {
+        inimigoAtual = 0;
+      }
+      inimigo.velocidade = parseInt(random(10, 30));
+    }
+
+    if (personagem.estaColidindo(inimigo)) {
+      image(imagemGameOver, width / 2 - 200, height / 3);
+      noLoop();
+    }
+  }
 }
--- sketch.js
+++ sketch.js
@@ -12,32 +12,5 @@ function keyPressed() {
 }
 
 function draw() {
-  cenario.exibe();
-  cenario.move();
-
-  pontuacao.exibe();
-  pontuacao.adicionarPonto();
-
-  personagem.exibe();
-  personagem.aplicaGravidade();
-
-  const inimigo = inimigos[inimigoAtual];
-  const inimigoVisivel = inimigo.x < -inimigo.largura;
-
-  inimigo.exibe();
-  inimigo.move();
-
-  if (inimigoVisivel) {
-    inimigoAtual++;
-
-    if (inimigoAtual > 2) {
-      inimigoAtual = 0;
-    }
-    inimigo.velocidade = parseInt(random(10, 30));
-  }
-
-  if (personagem.estaColidindo(inimigo)) {
-    image(imagemGameOver, width / 2 - 200, height / 3);
-    noLoop();
-  }
+  jogo.draw();
 }
