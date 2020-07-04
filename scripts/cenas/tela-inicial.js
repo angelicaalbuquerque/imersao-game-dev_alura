f61e8345efbc90b2355b780a6c9d323aeb7b4685
Angélica Albuquerque <angelica.o.albuquerque@gmail.com>
2020-07-03 23:12:56 -0300

    Texto tela inicial

--- scripts/cenas/tela-inicial.js
+++ scripts/cenas/tela-inicial.js
@@ -1,6 +1,22 @@
 class TelaInicial {
   constructor() {}
   draw() {
+    this._imagemDeFundo();
+    this._texto();
+  }
+
+  _imagemDeFundo() {
     image(imagemTelaInicial, 0, 0, width, height);
+  }
+
+  _texto() {
+    textFont(fonteTelaInicial);
+    textAlign(CENTER);
+
+    textSize(50);
+    text("As aventuras de", width / 2, height / 6);
+
+    textSize(200);
+    text("Hipsta", width / 2, (height / 8) * 3);
   }
 }
